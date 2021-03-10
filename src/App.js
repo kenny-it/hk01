6aa9b9956e8b4de3860b3c5f62c62e8be7c86ce9
kenny <kenny.ited@gmail.com>
2021-03-10 16:14:04 +0800

    update and reset the style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
+import React, { Fragment } from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
+import GlobalStyle from './GlobalStyle'
+import Iconfont from './static/iconfont'
 
 function App() {
   return (
-    <div>App</div>
+    <Fragment>
+      <GlobalStyle />
+      <Iconfont />
+      <Provider store={store}>
+       
+      </Provider>
+    </Fragment>
   );
 }
 
