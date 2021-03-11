ba250300076b8d670a95df6e2544165da1e1256e
kenny <kenny.ited@gmail.com>
2021-03-11 13:57:21 +0800

    change some style

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React, { Fragment } from 'react'
-import { Provider } from 'react-redux'
-import store from './store'
-import GlobalStyle from './GlobalStyle'
-import Iconfont from './static/iconfont'
 import Header from './common/header'
-import Login from './pages/login'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <Fragment>
-      <GlobalStyle />
-      <Iconfont />
-      <BrowserRouter>
-        <Provider store={store}>
-          <Switch>
-            <Route path="/" exact>
-              <Header />
+        <Header />
+        <Switch>
+            <Route path="/home" exact>
+              <div>Home</div>
             </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-          </Switch>
-        </Provider>
-      </BrowserRouter>
+        </Switch>
     </Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux'
+import store from './store'
+import GlobalStyle from './GlobalStyle'
+import Iconfont from './static/iconfont'
+import Login from './pages/login'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Fragment>
+      <GlobalStyle />
+      <Iconfont />
+      <BrowserRouter>
+        <Provider store={store}>
+          <Switch>
+            <Route path="/login" exact>
+              <Login />
+            </Route>
+            <Route path="/">
+              <App />
+            </Route>
+          </Switch>
+        </Provider>
+      </BrowserRouter>
+    </Fragment>
   </React.StrictMode>,
   document.getElementById('root')
 );
