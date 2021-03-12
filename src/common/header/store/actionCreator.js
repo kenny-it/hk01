0ebc11c677f0adfd7bc48e7eef577bbaeff4c1ef
kenny <kenny.ited@gmail.com>
2021-03-12 22:42:58 +0800

    used the axios to get api data

--- src/common/header/store/actionCreator.js
+++ src/common/header/store/actionCreator.js
@@ -1,4 +1,5 @@
 import * as constants from './constants'
+import axios from 'axios'
 
 export const getMoreAction = flag => {
     return {
@@ -10,5 +11,23 @@ export const getSearchBoxAction = flag => {
     return {
         type: constants.CHANGE_SEARCH_BOX_DISPLAY,
         flag
+    }
+}
+
+const getTitlesInfoChange = data => {
+    return {
+        type: constants.CHANGE_HEADER_TITLE_DATA,
+        data
+    }
+}
+
+export const getTitlesInfo = () => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.get('/api/header/info.json')
+            dispatch(getTitlesInfoChange(data.data))
+        } catch(err) {
+            console.log(err.message)
+        }
     }
 }
--- src/common/header/store/constants.js
+++ src/common/header/store/constants.js
@@ -1,3 +1,5 @@
 export const CHANGE_SHOW_MORE_DISPLAY = 'change_show_more_display'
 
 export const CHANGE_SEARCH_BOX_DISPLAY = 'change_search_box_display'
+
+export const CHANGE_HEADER_TITLE_DATA = 'change_header_title_data'
